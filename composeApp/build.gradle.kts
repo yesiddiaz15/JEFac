0611eef9487d6c0093d55a7c75f167b5196d06b1
Juan Felipe Garcia <70071051+JuanGarciaGomez@users.noreply.github.com>
2026-06-01 18:08:27 -0500

    Add splash

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -33,6 +33,7 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.core.splashscreen)
         }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
--- composeApp/src/androidMain/kotlin/com/yediaz/jefac/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/yediaz/jefac/MainActivity.kt
@@ -3,9 +3,11 @@ package com.yediaz.jefac
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent { App() }
     }
